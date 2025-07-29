284a81579d723282c869e55701f8ee538d286bdf
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-07-30 00:53:13 +0100

    Add dotenv file

--- server/app.js
+++ server/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import env from "dotenv";
 import { authRoute } from "./routers/authRouter.js";
 
+env.config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
