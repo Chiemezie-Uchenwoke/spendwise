5c9e3f81cba9b4adb5e2e60277867b9c85a3a6d3
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-08-01 15:58:24 +0100

    add login controller logic

---
+++ server/controllers/loginController.js
@@ -0,0 +1,47 @@
+import bcrypt from "bcrypt";
+import User from "../models/usermodel";
+import jwt from "jsonwebtoken";
+
+const jwtSecret = process.env.JWT_SECRET;
+
+const logInUser = async (req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+
+        const existingUser = await User.findOne({email});
+
+        if (!existingUser) {
+            return res.status(401).json({message: "Please register before login!"});
+        } 
+
+        if (existingUser) {
+            const match = await bcrypt.compare(password, existingUser.password);
+
+            if (match) {
+
+                const user = {
+                    userId: existingUser._id,
+                    username: existingUser.username
+                }
+
+                const token = jwt.sign(user, jwtSecret, {expiresIn: "15m"});
+                req.session.accessToken = token;
+
+                return res.status(200).json({
+                    success: true,
+                    message: "Login Successful!"
+                })
+            } else {
+                return res.status(403).json({message: "Wrong password!"});
+            }
+
+        }
+
+    } catch (err) {
+        console.log(err);
+        next(err);
+    }
+}
+
+export {logInUser};
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { registerUser } from "../controllers/registerController.js";
+import { logInUser } from "../controllers/loginController.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/register", registerUser);
+authRoute.post("/login", logInUser);
 
 export {authRoute};
