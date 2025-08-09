522923e8cad2e8a8aa2d31baa7375e4f76706488
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-08-09 03:22:44 +0100

    complete profile controller logic

---
+++ server/controllers/profileController.js
@@ -0,0 +1,56 @@
+import Profile from "../models/profileModel";
+
+const uploadProfileImage = async (req, res, next) => {
+    const {file} = req.file;
+
+    try {
+        const userId = req.user.userId;
+
+        if (!file){
+            return res.status(400).json({message: "No file uploaded"});
+        }
+
+        const filename = file.filename;
+
+        // check if profile already exist
+        const profile = await Profile.findOne({userId});
+
+        if (profile){
+            //update existing profile
+           await Profile.updateOne({userId}, {
+                profileImage: filename
+            });
+        } else {
+            await Profile.create({userId, profileImage: filename});
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Profile image uploaded successfully"
+        });
+
+    } catch(err){
+        next(err);
+    }
+}
+
+const getUserProfileImage = async (req, res, next) => {
+    try {
+        const userId = req.user.userId;
+        const profile = await Profile.findOne({userId});
+
+        if (!profile){
+            return res.status(404).json({message: "Profile image not found"});
+        }
+
+        return res.status(200).json({
+            success: true,
+            imageUrl: `/uploads/profiles/${profile.profileImage}`
+        });
+
+    } catch(err){
+        next(err);
+    }
+}
+
+export {uploadProfileImage, getUserProfileImage};
