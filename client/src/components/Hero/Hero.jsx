e367963b5961ec37e31d345be689aa367c7a11db
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-08-15 21:16:12 +0100

    complete hero component

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -3,16 +3,16 @@ import mobileChart from "../../assets/mobile_chart.jpg";
 
 const Hero = () => {
     return (
-        <section className="bg-pri-col min-h-[calc(100vh-4rem)] flex items-center justify-center py-12">
+        <section className="bg-pri-col min-h-[calc(100vh-4rem)] md:min-h-[calc(55vh-4rem)] lg:min-h-[calc(100vh-4rem)] flex items-center justify-center py-12">
             <div className="container h-full flex flex-col md:flex-row md:items-center gap-8">
                 <div className="flex flex-col gap-6 md:w-[50%]">
                     <h2 
-                      className="capitalize font-bold text-3xl text-white-col"
+                      className="capitalize font-bold text-3xl min-[1200px]:text-5xl text-white-col"
                     >
                         track your finances effortlessly
                     </h2>
 
-                    <p className="text-white-col">
+                    <p className="text-white-col min-[1200px]:text-lg/[2rem]">
                         Take control of your finances with our simple and intuitive expense tracking app. Add, edit, and achieve your financial goals.
                     </p>
 
@@ -32,11 +32,11 @@ const Hero = () => {
                     </div>
                 </div>
 
-                <div className="w-full h-[20rem] md:w-[50%]">
+                <div className="w-full h-full md:w-[50%]">
                     <img 
                       src={mobileChart} 
                       alt="image of a mobile phone showing a chart"
-                      className="w-full h-full object-cover rounded-md"
+                      className="w-full h-[60%] max-h-[20rem] object-cover rounded-md shadow-md"
                     />
                 </div>
             </div>
