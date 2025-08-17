7bfb5407e41da96fd7bbf58e7e4c4ed605362135
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-08-17 22:56:32 +0100

    complete login form component

--- client/src/components/LoginForm/LoginForm.jsx
+++ client/src/components/LoginForm/LoginForm.jsx
@@ -2,30 +2,61 @@ import { Link } from "react-router";
 
 const LoginForm = () => {
     return (
-        <form>
-            <div>
-                <h2>Login</h2>
-                <p>
-                    Provide your information to access your acount
-                </p>
+        <div className="w-full h-screen flex justify-center items-center">
+            <div className="container max-w-[25rem] h-auto border border-black/20 bg-white-shade mx-auto my-8 py-8 px-4 rounded-md flex flex-col gap-6">
+                <div className="flex flex-col gap-2">
+                    <h2 className="capitalize font-bold text-base md:text-lg lg:text-xl">Login</h2>
+
+                    <p className="text-sm text-black/50">
+                        Provide your information to access your acount
+                    </p>
+                </div>
+
+                <form className="flex flex-col gap-5">
+                    <div className="flex flex-col gap-2">
+                        <label 
+                        htmlFor="email" 
+                        className="font-medium"
+                        >
+                            Email
+                        </label>
+
+                        <input 
+                        type="email" 
+                        id="email" 
+                        placeholder="name@example.com" 
+                        className="border border-black/20 w-full h-[2.5rem] rounded-md px-2 text-sm focus:border-2 focus:border-black lowercase"
+                        />
+                    </div>
+
+                    <div className="flex flex-col gap-2">
+                        <label 
+                        htmlFor="password"
+                        className="font-medium"
+                        >
+                            Password
+                        </label>
+
+                        <input 
+                        type="password" 
+                        id="password" 
+                        placeholder="Enter password"
+                        className="border border-black/20 w-full h-[2.5rem] rounded-md px-2 text-sm focus:border-2 focus:border-black "
+                        />
+                    </div>
+
+                    <button className="bg-pri-col text-white-col py-2 rounded-md font-semibold capitalize">Login</button>
+                </form>
+
+                <div className="flex flex-col gap-4">
+                    <p className="text-xs">
+                        Don't have an account? <Link to="/register" className="text-pri-col font-medium hover:brightness-90">Sign up</Link>
+                    </p>
+
+                    <Link to="/" className="text-sec-col capitalize text-xs font-semibold hover:underline">back to home page</Link>
+                </div>
             </div>
-
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" id="email" placeholder="name@example.com" />
-            </div>
-
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" id="password" />
-            </div>
-
-            <button>Login</button>
-
-            <p>
-                Don't have an account? <Link to="/register">Sign up</Link>
-            </p>
-        </form>
+        </div>
     )
 }
 
