9d8e136c3c3f26e52be4662cdcbfb9122233b90d
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-08-23 15:25:02 +0100

    Add user object to dashboard

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,18 +1,25 @@
 import { MdDashboard } from "react-icons/md";
 import { FaUpload, FaArrowTrendUp, FaArrowRightFromBracket } from "react-icons/fa6";
 import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { useAuth } from "../../hooks/useAuth";
 
 const Dashboard = () => {
+    const {user} = useAuth();
+
     return (
         <div className="w-full h-[calc(100vh-3.8rem)] min-[1000px]:h-[calc(100vh-4rem)] relative ">
-            <div className="w-full h-full flex">
+            <div className="w-full h-full flex gap-4">
                 <aside 
                     className="w-[65%] max-w-[15rem] min-[900px]:w-[25%] h-full shadow-lg absolute left-0 top-0 min-[900px]:static min-[900px]:border-r border-black/20 flex flex-col justify-between p-3 z-20 bg-white-col overflow-y-auto"
                 >
                     <div className="flex flex-col gap-8">   
                         <div className="flex flex-col gap-1">
                             <h2 className="font-bold text-sm">Welcome</h2>
-                            <p className="text-xs capitalize">Username</p>
+                            <p className="text-sm capitalize">
+                                {
+                                    user ? user?.username : null
+                                }
+                            </p>
                         </div>
 
                         <div className="flex flex-col gap-3">
@@ -20,7 +27,7 @@ const Dashboard = () => {
                                 add transaction
                             </button>
 
-                            <h4 className="text-sm capitalize flex items-center gap-2 text-pri-col bg-pri-col/10 py-2 px-4 rounded-md text-center">
+                            <h4 className="text-sm capitalize flex items-center gap-2 text-pri-col bg-pri-col/10 py-2 px-4 rounded-md text-center select-none">
                                 <MdDashboard className="text-pri-col" /> 
                                 dashboard
                             </h4>
