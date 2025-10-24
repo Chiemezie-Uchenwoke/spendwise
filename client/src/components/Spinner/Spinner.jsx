bf2c74e2a8bf35f6edf3edd6d80135702814b6e1
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-10-24 10:58:53 +0100

    Add Spinner component and use loading state to properly load dashboard page

---
+++ client/src/components/Spinner/Spinner.jsx
@@ -0,0 +1,7 @@
+const Spinner = () => (
+  <div className="flex items-center justify-center h-screen">
+    <div className="w-10 h-10 border-4 border-gray-300 border-t-pri-col rounded-full animate-spin"></div>
+  </div>
+);
+
+export default Spinner;
--- client/src/utils/ProtectedRoute.jsx
+++ client/src/utils/ProtectedRoute.jsx
@@ -1,7 +1,13 @@
 import { Navigate, Outlet } from "react-router";
 import { useAuth } from "../hooks/useAuth";
+import Spinner from "../components/Spinner/Spinner";
 
 export default function ProtectedRoute() {
-  const { user } = useAuth();
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <Spinner />;
+  }
+
   return user ? <Outlet /> : <Navigate to="/login" replace />;
 }
